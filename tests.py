9274bfbcd213668e9baaf2a3477114f89730dda1
Alexander Tarancon <ataranco@umail.iu.edu>
2019-12-19 16:13:35 -0500

    added pytest coverage , and created users blueprint route

---
+++ tests.py
@@ -0,0 +1,39 @@
+
+from datetime import datetime, timedelta
+import unittest
+from app import create_app,db
+from app.models import User, Post
+from config.setting import Config
+
+class TestConfig(Config):
+    TESTING = True
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+    
+
+
+    def test_password_hashing(self):
+        u = User(username='susan',password='cat', email='susan@gmail')
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password('cat'))
+    
+
+  
+
+
+if __name__ == '__main__':
+    print("hello")
+    unittest.main(verbosity=2)
---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest 
+from app import create_app
+
+
+@pytest.yield_fixture(scope ='session')
+def app():
+    """
+    Set up our flask test app, this gets executed only once
+    :return: Flask app
+    """
+    params = {
+        'DEBUG': False,
+        'TESTING': True,
+    }
+
+    _app = create_app(settings_override=params)
+
+    #Establish an application context before running the tests.
+    ctx = _app.app_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+
+@pytest.yield_fixture(scope='function')
+def client(app):
+    """
+    Setup an app client, this gets executed for each test function 
+
+    :param app: Pytest fixture
+    :return: FLask app client
+    """
+
+    yield app.test_client()
+
