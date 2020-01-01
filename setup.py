e2a0b30c142c724ee8d6b2c20c0a506ea0d45f62
Alexander Tarancon <ataranco@umail.iu.edu>
2020-01-01 13:10:04 -0500

    click commmand  package  basic layout  working

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup 
 import sys 
+sys.path.append("/Users/at/Desktop/Home/flask-mega-tutorial/microblog/cli_package")
 
 for p in sys.path:
     print(p)
@@ -7,13 +8,13 @@ for p in sys.path:
 setup(
     name="microblog-CLI",
     version='1.0',
-    packages=['cli'],
+    packages=['cli_package'],
     include_package_data=True,
     install_requires=[
         'click',
     ],
     entry_points="""
-    [console_scripts]
-    cli_main=cli.cli_main:cli
+        [console_scripts]
+        cli_main=cli_package.cli_main:hello
     """,
 )
