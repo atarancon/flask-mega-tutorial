552084c1855e4e2d5cdedc8df2471eb1f81f4c88
Alexander Tarancon <ataranco@umail.iu.edu>
2020-01-04 14:11:02 -0500

    testing for user views

--- tests/user/test_views.py
+++ tests/user/test_views.py
@@ -1,6 +1,7 @@
 from flask import url_for
 from app.models import User
 from lib.tests import assert_status_with_message, ViewTestMixin
+from config.setting import TestConfig
 
 
 class TestLogin(ViewTestMixin):
@@ -32,6 +33,60 @@ class TestSignup(ViewTestMixin):
         """ Signup renders succesfully """
         response = self.client.get(url_for('users.signup'))
         assert response.status_code==200
+    
+    def test_begin_signup_fail(self):
+        """ Signup failure due to using an account that already exists. """
+        user = { 'email' : TestConfig.SEED_ADMIN_EMAIL , 'username:' : "admin", 'password' : TestConfig.SEED_ADMIN_PASSWORD ,
+               'pass_conf' : TestConfig.SEED_ADMIN_PASSWORD }
+        response = self.client.post(url_for('users.signup'), data=user , follow_redirects=True)
+
+        assert_status_with_message(200 , response ,"Your Email has been already register")
+
+    def test_begin_signup_username_fail(self):
+        """ Sign up failure due to inputting an username already exist"""
+
+        user = { 'email' : "foo@foo.com" , 'username' : "admin", 'password' : TestConfig.SEED_ADMIN_PASSWORD ,
+               'pass_conf' : TestConfig.SEED_ADMIN_PASSWORD }
+        response = self.client.post(url_for('users.signup'), data=user , follow_redirects=True)
+
+        assert_status_with_message(200 , response ,"Your username has been already register")
+    
+    def test_signup(self, users):
+        """ Sign up succesfully """
+        old_user_count = User.query.count()
+
+        user = { 'email': "new@local.host" , 'username' : "newuser" , 'password': 'password' , 
+                    'pass_conf': 'password'}
+        response = self.client.post(url_for("users.signup"), data=user, follow_redirects=True)
+
+        assert_status_with_message( 200, response, "Awesome, Thanks for registering")
+
+        # new user got addded to table and increment number of users by one
+        new_user_count = User.query.count()
+        assert (old_user_count + 1) == new_user_count
+
+        #ensure password has been hashed 
+        new_user = User.find_by_identity('new@local.host')
+        assert new_user.password_hash != 'password'
+
+    def test_redirect_signup(self, users):
+        """ Sign up makes it to login page """
+        user = { 'email': "new@local.host" , 'username' : "newuser" , 'password': 'password' , 
+                    'pass_conf': 'password'}
+        response = self.client.post(url_for("users.signup"), data=user, follow_redirects=False)
+        assert response.status_code == 302
+
+        print(response['Location'])
+
+
+
+
+        
+
+
+
+        
+
     
 
     
