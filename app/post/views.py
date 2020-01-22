5fdb6392b58ed414c90bb4ac7077ff8c6c426120
Alexander Tarancon <ataranco@umail.iu.edu>
2020-01-22 18:40:18 -0500

    added search form for post

--- app/post/views.py
+++ app/post/views.py
@@ -14,21 +14,25 @@ from flask import (
 
 from . import post
 
+from app.post.forms import SearchForm
+
 
 #display all blogs list
 @post.route('/posts' , defaults = {'page'  : 1} )
 @post.route('page/<int:page>')
 def posts(page):
 
+    search_form = SearchForm()
     paginated_posts = Post.query.filter(Post.search(text(request.args.get('q','')))).order_by(Post.timestamp.desc()).paginate(page,50,True)
     print("hello posts")
-    return render_template('post/index.html' , posts = paginated_posts)
+    return render_template('post/index.html' , posts = paginated_posts , form= search_form)
 
 #display single post 
 @post.route('single_post/<int:post_id>')
 def single_post(post_id):
+    
     blog_post = Post.query.get_or_404(post_id)
-    return render_template('post/post.html',post = blog_post)
+    return render_template('post/post.html',post = blog_post )
 
 
 
