147e54169cf3cabff3c3ff14be1f494a57c80f1e
Alexander Tarancon <ataranco@umail.iu.edu>
2020-01-22 18:42:04 -0500

    post searchable keywords feature

---
+++ app/post/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField
+
+from wtforms.validators import Optional, Length,DataRequired
+
+
+
+
+
+
+class SearchForm(FlaskForm):
+    q = StringField('Search Terms', [Optional(), Length(1, 256)])
