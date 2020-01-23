098fa455026d987153b7c4f81653845b4f987998
Alexander Tarancon <ataranco@umail.iu.edu>
2020-01-23 14:48:21 -0500

    test delete route in post

--- tests/post/test_views.py
+++ tests/post/test_views.py
@@ -34,5 +34,54 @@ class TestPostNew(ViewTestMixin):
         new_count = Post.query.count()
         assert old_count + 1 == new_count
 
+class TestPostEdit(ViewTestMixin):
+    def test_post_edit(self):
+
+        params = {
+             'title' : "Number One",
+             'body' : "At everything"
+        }
+        
+        self.login()
+
+        response = self.client.post( url_for('admin.post_new') , data=params , follow_redirects=True)
+
+        assert_status_with_message(200 , response , "post has been added")
+        assert Post.query.filter(Post.title==params['title'])
+        id = Post.query.filter(Post.title==params['title']).all().pop().id
+
+        e_param = {
+            'title' : "edit One",
+            'body' : "edit everything"
+        }
+
+        #call edit route 
+        response = self.client.post( url_for('admin.post_edit' , id = id) , data= e_param , follow_redirects=True)
+        assert Post.query.filter(Post.title==e_param['title'])
+        assert_status_with_message(200 , response , "Post has been updated successfully")
+
+
+
+class TestDeletePost(ViewTestMixin):
+    def test_delete_post(self):
+        params = {
+             'title' : "Number One",
+             'body' : "At everything"
+        }
+        
+        self.login()
+
+        response = self.client.post( url_for('admin.post_new') , data=params , follow_redirects=True)
+
+        assert_status_with_message(200 , response , "post has been added")
+        assert Post.query.filter(Post.title==params['title'])
+        post = Post.query.filter(Post.title==params['title']).all().pop()
+        id = post.id
+        response = self.client.post( url_for('admin.post_delete', post_id = id), follow_redirects = True)
+
+        assert_status_with_message ( 200 , response,"Post title:{0} deleted successfulyl " .format(post.title) )
+
+
+
 
 
