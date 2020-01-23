bcffa59e007a59f23a1e2f5d63cd9dcecb6e017b
Alexander Tarancon <ataranco@umail.iu.edu>
2020-01-22 19:03:59 -0500

    search edge case (when no result availble or no posts available)

--- app/post/forms.py
+++ app/post/forms.py
@@ -4,9 +4,5 @@ from wtforms import StringField
 from wtforms.validators import Optional, Length,DataRequired
 
 
-
-
-
-
 class SearchForm(FlaskForm):
     q = StringField('Search Terms', [Optional(), Length(1, 256)])
